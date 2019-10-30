0dc9a78f22d18942916fc5dd21ca758bb5c82ee0
mamenghua <1450125019@qq.com>
2019-10-30 10:15:55 +0800

    01home

---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>欢迎登录首家后台管理系统</h1>
+    <p>请登录</p>
+    <button @click="tap()">登录</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  methods:{
+    tap(){
+      this.$router.push('/Home')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/SearchUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>用户查询</h2>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'SearchUser'
+
+}
+</script>
+
+<style scoped>
+</style>
