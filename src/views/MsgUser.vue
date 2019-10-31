79e4d2daacd7310bba89419d6f3e953e8b975b97
yuanziheng <710691246@qq.com>
2019-10-31 08:09:26 +0800

    1

--- src/views/MsgUser.vue
+++ src/views/MsgUser.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <h2>用户信息</h2>
+    <div>
+        <el-input placeholder="请输入用户id" v-model="input">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+    </div>
   </div>
 </template>
 
 <script>
 export default{
-  name:'MsgUser'
-
+  name:'MsgUser',
+  data(){
+    return{
+      input:''
+    }
+  }
 }
 </script>
 
--- src/views/SearchUser.vue
+++ src/views/SearchUser.vue
@@ -1,15 +1,95 @@
 <template>
   <div>
-    <h2>用户查询</h2>
+    <template>
+      <el-table
+        :data="tableData"
+        :cell-style="cellStyle" :header-cell-style="rowClass"
+        stripe
+        style="width: 100%">
+        <el-table-column
+          prop="_id"
+          label="用户ID"
+          width="320">
+        </el-table-column>
+        <el-table-column
+          prop="userName"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="nickName"
+          label="昵称">
+        </el-table-column>
+        <el-table-column
+          prop="createdAt"
+          label="创建时间">
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          >
+          <template slot-scope="scope">
+            <el-button type="danger" icon="el-icon-delete" circle @click="handleDelete(scope.row._id)"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+    </template>
   </div>
 </template>
 
 <script>
+import * as api from '../api/getUser.js'
+import * as API from '../api/deleteUser.js'
 export default{
-  name:'SearchUser'
-
+  name:'SearchUser',
+  data(){
+    return{
+      input1:'',
+      token:localStorage.getItem('token'),
+      id:'',
+      tableData:[]
+    }
+  },
+  mounted(){
+    this.$axios.get("http://api.cat-shop.penkuoer.com/api/v1/admin/users?",{
+      headers:{'authorization':'Bearer '+this.token}
+    }).then((data)=>{
+      // this.id = data.data.users[0]._id
+      // console.log(data.data.users)
+      this.tableData=data.data.users     //console.log(this.list)
+    })
+  },
+  methods: {
+    cellStyle({ row, column, rowIndex, columnIndex }){
+      return 'text-align:center';
+    },
+    rowClass({ row, rowIndex }){
+      return 'text-align:center;line-height:50px';
+    },
+    handleDelete(index) {
+      API.deleteUser(localStorage.getItem('token'),{index}).then((data)=>{
+        console.log(data)
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .el-input{
+    height: 50px;
+    line-height: 50px;
+    width: 20%;
+  }
+  .el-input__inner{
+    width: 20%;
+  }
+  table .el-table td,table .el-table th{
+    text-align: center;
+  }
+  .el-row{
+    height: 50px;
+    position: relative;
+    line-height: 10px;
+  }
 </style>
